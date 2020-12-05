6a8c348757da019eaa7ad176cdd8f41e5dba6176
alexandrajulius <alexandra.julius@gmail.com>
2020-12-05 14:06:52 +0100

    solves second puzzle of fifth day

--- src/Day05.php
+++ src/Day05.php
@@ -5,7 +5,31 @@ declare(strict_types=1);
 final class Day05
 {
     // Boarding Passes
+    public function findMySeatId(string $rawInput): int
+    {
+        $seats = $this->findAllSeatIds($rawInput);
+        sort($seats);
+
+        $mySeatId = 0;
+        for ($i = 1; $i < (count($seats) - 1); $i++) {
+            if ($seats[$i - 1] == ($seats[$i] - 2)) {
+                $mySeatId = $seats[$i] - 1;
+            }
+        }
+
+        return $mySeatId;
+    }
+
+
     public function findHighestSeatId(string $rawInput): int
+    {
+        $seats = $this->findAllSeatIds($rawInput);
+        rsort($seats);
+
+        return $seats[0];
+    }
+
+    private function findAllSeatIds(string $rawInput): array
     {
         $boardingPasses = explode("\n", $rawInput);
 
@@ -13,9 +37,8 @@ final class Day05
         foreach ($boardingPasses as $boardingPass) {
             $seats[] = $this->findSeat($boardingPass);
         }
-        rsort($seats);
 
-        return $seats[0];
+        return $seats;
     }
 
     public function findSeat(string $boardingPass): int
