9eb3608efe2d1fa4a1b3b494790a6ad29fa878b2
alexandrajulius <alexandra.julius@gmail.com>
2020-12-06 11:15:14 +0100

    solves second puzzle of sixth day

--- src/Day06.php
+++ src/Day06.php
@@ -4,35 +4,66 @@ declare(strict_types=1);
 
 final class Day06
 {
-    public function getAnswersPerGroup(string $rawInput): int
+    public function getAmountOfCommonAnswers(string $rawInput): int
     {
-        $allGroupsAnswers = $this->convertInputData($rawInput);
+        $allGroupsAnswers = $this->convertInput($rawInput);
+        $allUniqueYesAnswers = [];
+        foreach ($allGroupsAnswers as $groupsAnswers) {
+            $allUniqueYesAnswers[] = $this->getAmountOfCommonYesAnswersPerGroup($groupsAnswers);
+        }
 
-        return array_sum($allGroupsAnswers);
+        return array_sum($allUniqueYesAnswers);
     }
 
-    private function convertInputData(string $rawInput): array
+    public function getAmountOfAnswers(string $rawInput): int
+    {
+        $allGroupsAnswers = $this->convertInput($rawInput);
+        $allYesAnswers = [];
+        foreach ($allGroupsAnswers as $groupsAnswers) {
+            $allYesAnswers[] = $this->getAmountOfYesAnswersPerGroup($groupsAnswers);
+        }
+
+        return array_sum($allYesAnswers);
+    }
+
+    private function convertInput(string $rawInput): array
     {
         $allGroupsAnswers = explode("\n\n", $rawInput);
 
         $output = [];
         foreach ($allGroupsAnswers as $answersPerGroup) {
-            $output[] = $this->getAmountOfYesAnswers(explode("\n", $answersPerGroup));
+            $output[] = explode("\n", $answersPerGroup);
         }
 
         return $output;
     }
 
-    public function getAmountOfYesAnswers(array $answersPerGroup): int
+    private function getAmountOfYesAnswersPerGroup(array $answersPerGroup): int
     {
         $flattenedGroupAnswers = '';
-        foreach ($answersPerGroup as $answers) {
-            $flattenedGroupAnswers .= $answers;
+        foreach ($answersPerGroup as $answersPerPerson) {
+            $flattenedGroupAnswers .= $answersPerPerson;
         }
 
         $unique = array_unique(str_split($flattenedGroupAnswers));
         $amount = array_count_values($unique);
 
         return array_sum($amount);
+    }
+
+    public function getAmountOfCommonYesAnswersPerGroup(array $answersPerGroup): int
+    {
+        $answersPerPersonArray = [];
+        foreach ($answersPerGroup as $answersPerPersonString) {
+            $answersPerPersonArray[] = str_split($answersPerPersonString);
+        }
+
+        if (count($answersPerPersonArray) == 1) {
+            return count($answersPerPersonArray[0]);
+        }
+
+        $result = array_intersect(...$answersPerPersonArray);
+
+        return count($result);
     }
 }
