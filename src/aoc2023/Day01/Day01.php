e503e7a9c16540e20fbc2824a392cba06603e324
Alex <alexandra.julius@gmail.com>
2023-12-02 11:53:28 +0100

    day02, 2023

--- src/aoc2023/Day01/Day01.php
+++ src/aoc2023/Day01/Day01.php
@@ -6,18 +6,6 @@ namespace aoc2023\Day01;
 
 final class Day01
 { 
-    private const WORDS = [
-        'one' => 1,
-        'two' => 2,
-        'three' => 3,
-        'four' => 4,
-        'five' => 5,
-        'six' => 6,
-        'seven' => 7,
-        'eight' => 8,
-        'nine' => 9
-    ];
-
     public function firstTask(string $input): int
     {
         $calibrationDoc = $this->getArrayOfStrings($input);
